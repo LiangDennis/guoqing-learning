7a999004c13dd7676ec935b48357345a37eef1f5
LiangDennis <18898513421@163.com>
2019-10-01 20:47:59 +0800

    补充双大括号

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,15 +3,27 @@
 Page({
   data:{
     name:"你好",
-    counter:0
+    counter:0,
+    provider:[
+      {id:'01',name:'广州城投',address:'广州黄埔'},
+      {id:'02',name:'广州黄埔科信',address:'广州黄埔'},
+      {id:'03',name:'广州蓝亚科技',address:'广州黄埔'},
+      {id:'04',name:'广州加勒比数据',address:'广州黄埔'},
+      {id:'05',name:'杭州溪塔',address:'杭州'}
+    ],
+    person: {
+      id:1,
+      name:"zhangzhang",
+      age:101
+    }
   },
   changeCounter(event) {
-    // console.log(event.currentTarget.id);
-    if(event.currentTarget.id === "add") {
+    console.log(event);
+    if(event.target.id === "add") {
       this.setData({
         counter:this.data.counter+1
       })
-    }else if(event.currentTarget.id === "sub") {
+    } else if (event.target.id === "sub") {
       this.setData({
         counter:this.data.counter-1
       })
